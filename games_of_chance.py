0be60c211ef70e81d646f4a916d0ff73da89dcbd
Nakul <nakul.gopakumar@gmail.com>
2020-07-18 14:00:36 +0530

    added code to determine winner

--- games_of_chance.py
+++ games_of_chance.py
@@ -1,10 +1,21 @@
 import random
 
+
 #function to roll the dice and return a number
 def roll_dice():
     num=random.randint(1,6)
     return num
 
-choice = roll_dice()
-print("You rolled .......:   " + str(choice))
+print("Starting the game....\n You and computer both have 100$ in hand.")
+print("You can increase the bet amount for every roll after this....\n\n")
+print("....\n Call your dice..  (any number betweeen 1 and 6) ")
+choice = int(input())
+print("\nRolling....")
+rolled = roll_dice()
+print("You rolled .......:   " + str(rolled))
+
+if choice == rolled:
+    print("You won")
+else:
+    print("You lost")
 
