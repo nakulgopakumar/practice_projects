ef8f778ad5d3cd2c5a2a3f6c7a039d57d3cf70a1
Nakul <nakul.gopakumar@gmail.com>
2020-07-18 20:52:52 +0530

    Added conditonal checks for bet validity

--- games_of_chance.py
+++ games_of_chance.py
@@ -3,9 +3,13 @@ bet_amount = [100, 100]
 
 
 #function to update bet amount of user and computer
-def bet ():
+def bet (begin):
     print("Enter the amount you want to bet......\n")
     bet_by_user = int(input())
+    if begin==0 and bet_by_user>100:
+        print("Please enter a value within 100$ \n")
+        print("Enter the amount you want to bet......\n")
+        bet_by_user = int(input())
     bet_by_computer = computer_bet()
     print("\nThe computer has bet....." + str(bet_by_computer))
     bet_amount[0] = bet_amount[0]-bet_by_user
@@ -35,10 +39,14 @@ def roll_dice():
     return num
 
 game = 1
+begin = 0
 while game == 1:
 #starting game and updating bets
-    print("Starting the game....\n You and computer both have 100$ in hand.")
-    bet_u,bet_c = bet()
+    if begin == 0:
+        print("Starting the game....\n You and computer both have 100$ in hand.")
+    elif begin == 1:
+        print("You seem confident. Here we go again....!!\n")
+    bet_u,bet_c = bet(begin)
 
     #rolling dice
     print("....\n Call your dice..  (any number betweeen 1 and 6) ")
@@ -53,6 +61,11 @@ while game == 1:
     #current winnings
     print("Your money : " + str(bet_amount[0]) + "$")
     print("Computer's money : " + str(bet_amount[1]) + "$")
+    begin = 1
+    if(bet_amount[0]<=0):
+        print("You have lost all your money")
+        break
+    
 
     print("\n\n Press 1 to continue. Press 2 to exit")
     game = int(input())
